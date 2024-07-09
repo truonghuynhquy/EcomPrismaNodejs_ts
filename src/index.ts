ca414577ee0a995c6837ea0e973ae94972c93899
Truong Huynh Quy <quytruong2509@gmail.com>
2024-07-09 09:31:38 +0700

    Complete: Sign Up

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import { PORT } from "./secrets";
 import rootRouter from "./routes";
+import { PrismaClient } from "@prisma/client";
 
 const app: Express = express();
 
+app.use(express.json());
 app.use("/api", rootRouter);
+
+export const prismaClient = new PrismaClient({
+  log: ["query"],
+});
 
 app.listen(PORT, () => console.log("App working!"));
