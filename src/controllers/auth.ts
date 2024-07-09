6eb30cb559f946042c3deb8f096610d1892de2d0
Truong Huynh Quy <quytruong2509@gmail.com>
2024-07-09 09:55:37 +0700

    Complete: Login

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express";
 import { prismaClient } from "..";
-import { hashSync } from "bcrypt";
+import { hashSync, compareSync } from "bcrypt";
+import * as jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../secrets";
 
 export const signup = async (req: Request, res: Response) => {
   const { email, password, name } = req.body;
 
   let user = await prismaClient.user.findFirst({ where: { email } });
+  console.log("USER", user);
   if (user) {
     throw Error("User already exists");
   }
@@ -19,4 +22,21 @@ export const signup = async (req: Request, res: Response) => {
   });
 
   res.json(user);
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  let user = await prismaClient.user.findFirst({ where: { email } });
+  if (!user) {
+    throw Error("User does not exists");
+  }
+
+  if (!compareSync(password, user.password)) {
+    throw Error("Passwords do not match");
+  }
+
+  const token = jwt.sign({ URLserId: user.id }, JWT_SECRET);
+
+  res.json({ user, token });
 };
