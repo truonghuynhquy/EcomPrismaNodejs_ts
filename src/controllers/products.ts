3114c0f51a54bde08659f679307519278f9524ba
Truong Huynh Quy <quytruong2509@gmail.com>
2024-07-17 16:16:01 +0700

    Add: Update Product

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,5 +1,7 @@
+import { ErrorCode } from "./../exceptions/root";
 import { Request, Response } from "express";
 import { prismaClient } from "..";
+import { NotFoundException } from "../exceptions/not-found";
 
 export const createProduct = async (req: Request, res: Response) => {
   // ["tea, "india"] => "tea, india"
@@ -15,7 +17,26 @@ export const createProduct = async (req: Request, res: Response) => {
   res.json(product);
 };
 
-export const updateProduct = async (req: Request, res: Response) => {};
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const product = req.body;
+    if (product.tags) {
+      product.tags = product.tags.join(",");
+    }
+    const updateProduct = await prismaClient.product.update({
+      where: {
+        id: +req.params.id,
+      },
+      data: product,
+    });
+    res.json(updateProduct);
+  } catch (error) {
+    throw new NotFoundException(
+      "Product not found",
+      ErrorCode.PRODUCT_NOT_FOUND
+    );
+  }
+};
 export const deleteProduct = async (req: Request, res: Response) => {};
 export const listProducts = async (req: Request, res: Response) => {};
 export const getProductById = async (req: Request, res: Response) => {};
