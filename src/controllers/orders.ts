69d206b39b8432f051141305f37021310f30fcb5
Truong Huynh Quy <quytruong2509@gmail.com>
2024-08-08 17:49:58 +0700

    Complete: Order

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { prismaClient } from "..";
+import { NotFoundException } from "../exceptions/not-found";
+import { ErrorCode } from "../exceptions/root";
 
 export const createOrder = async (req: Request, res: Response) => {
   // 1. To create a transaction
@@ -57,6 +59,51 @@ export const createOrder = async (req: Request, res: Response) => {
     return res.json(order);
   });
 };
-export const listOrders = async (req: Request, res: Response) => {};
-export const cancelOrder = async (req: Request, res: Response) => {};
-export const getOrderById = async (req: Request, res: Response) => {};
+export const listOrders = async (req: Request, res: Response) => {
+  const orders = await prismaClient.order.findMany({
+    where: {
+      userId: req.user?.id,
+    },
+  });
+  res.json(orders);
+};
+export const cancelOrder = async (req: Request, res: Response) => {
+  // 1. Wrap it inside transaction
+  // 2. Check if the users is cancelling its own order
+
+  try {
+    const order = await prismaClient.order.update({
+      where: {
+        id: +req.params.id,
+      },
+      data: {
+        status: "CANCELLED",
+      },
+    });
+    await prismaClient.orderEvent.create({
+      data: {
+        orderId: order.id,
+        status: "CANCELLED",
+      },
+    });
+    res.json(order);
+  } catch (error) {
+    throw new NotFoundException("Order not found", ErrorCode.ORDER_NOT_FOUND);
+  }
+};
+export const getOrderById = async (req: Request, res: Response) => {
+  try {
+    const order = await prismaClient.order.findFirstOrThrow({
+      where: {
+        id: +req.params.id,
+      },
+      include: {
+        products: true,
+        events: true,
+      },
+    });
+    res.json(order);
+  } catch (error) {
+    throw new NotFoundException("Order not found", ErrorCode.ORDER_NOT_FOUND);
+  }
+};
